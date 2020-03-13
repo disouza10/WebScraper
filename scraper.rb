73d64454fa32a6f18a425767d995982c4b0a0c1a
Diego Souza <diego@rupee.com.br>
2020-03-13 01:57:47 -0300

    browser now is in full width, does not load images nor closes after script is done

--- scraper.rb
+++ scraper.rb
@@ -1,28 +1,44 @@
 require 'nokogiri'
-require 'httparty'
 require 'pry'
 require 'mechanize'
 require 'watir'
 require 'webdrivers'
 
-def scraper
-  # set the chrome driver in specific path
+def set_variables
+  @nome_bairro = 'Barra da Tijuca'
+end
+
+def set_webdriver
   Selenium::WebDriver::Chrome::Service.driver_path=File.join(Dir.pwd, 'chromedriver.exe')
-  browser = Watir::Browser.new
+  
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_option(:detach, true)
+  options.add_argument("start-maximized")
+  options.add_preference('webkit.webprefs.loads_images_automatically', false)
+
+  @browser = Watir::Browser.new :chrome, :options => options
+end
+
+def scraper
+  set_variables
+  set_webdriver
   
   url = 'https://www.vivareal.com.br/'
-  browser.goto(url)
+  @browser.goto(url)
   
-  form = browser.form(array: 'main-search__form js-base-search')
+  form = @browser.form(class: %w(main-search__form js-base-search))
   return false unless form.exist?
-  # seleciona o alugar
+  
   form.select_list(class: 'js-select-business').option(value: 'rent').select
-  # seleciona o tipo apartamento
+  # checar se é possível pegar o select apenas se ele contiver uma palavra do value, como por exemplo
+  # se value contém apartment, então seleciona essa opção
   form.select_list(class: 'js-select-type').option(value: 'APARTMENT|UnitSubType_NONE,DUPLEX,LOFT,STUDIO,TRIPLEX|RESIDENTIAL|APARTMENT').select
-  # escreve catete na busca textual
-  form.text_field.set('catete')
-  # primeiro elemento, que é o bairro
-  form.element(xpath: '//*[@id="js-site-main"]/section[1]/div/div/form[1]/div[2]/div/div/div/div/div/ul[1]/li').click
+  form.text_field.set(@nome_bairro)
+
+  bairro_element = form.li(data_type: 'neighborhood')
+  if bairro_element.text.split(', ')[0].downcase == @nome_bairro.downcase
+    bairro_element.click
+  end
 end
 
 scraper
