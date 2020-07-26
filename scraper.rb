96e8e2a930f333c858a5071b27540708dedea659
Diego Souza <diego@rupee.com.br>
2020-07-26 02:50:09 -0300

    viva real done

--- scraper.rb
+++ scraper.rb
@@ -3,13 +3,16 @@ require 'pry'
 require 'mechanize'
 require 'watir'
 require 'webdrivers'
+require 'caxlsx'
+require "i18n"
+I18n.config.available_locales = :en
 
 def set_variables
-  @neighborhood_name = 'Catete'
+  @neighborhood_name = 'copacabana'
   @city_name = 'Rio de Janeiro'
   @bedrooms = 2
   @min_price = 0
-  @max_price = 2000
+  @max_price = 3000
 end
 
 def set_webdriver
@@ -19,7 +22,7 @@ def set_webdriver
   options = Selenium::WebDriver::Chrome::Options.new
   # options.add_option(:detach, true)
   options.add_argument("start-maximized")
-  # options.add_preference('webkit.webprefs.loads_images_automatically', false)
+  options.add_preference('webkit.webprefs.loads_images_automatically', false)
 
   @browser = Watir::Browser.new :chrome, :options => options
 end
@@ -42,28 +45,29 @@ def viva_real
 
   neighborhood_elements = form.lis(data_type: 'neighborhood')
   neighborhood_elements.each do |ne|
-    if (ne.text.split(', ')[0].downcase == @neighborhood_name.downcase) && ( ne.text.split(', ')[1].split(' -')[0].downcase == @city_name.downcase)
+    if (I18n.transliterate(ne.text.split(', ')[0]).downcase == @neighborhood_name.downcase) && ( I18n.transliterate(ne.text.split(', ')[1].split(' -')[0]).downcase == @city_name.downcase)
       ne.click
       break
     end
   end
   sleep(2)
   
-  binding.pry
   form2 = @browser.form
   # min_price = form2.div(class: 'filter-range__container').text_field(id: 'filter-range-from-price')
   # min_price.set(@min_price)
   max_price = form2.div(class: 'filter-range__container').text_field(id: 'filter-range-to-price')
   max_price.set(@max_price)
   form2.div(class: %w(form__pills js-bedrooms-quantity)).ul(class: %w(filter-pills js-container)).button(data_value: @bedrooms.to_s).click
-  sleep(2)
+  sleep(3)
   
-  total_pages = @browser.lis(class: 'pagination__item', data_type: 'number').length
+  total_pages = @browser.lis(class: 'pagination__item', data_type: 'number').last.link.data_page.to_i
+  pagina_atual = @browser.link(class: 'js-change-page', data_active: true).data_page.to_i
+  proxima_pagina = @browser.link(class: 'js-change-page', data_page: (pagina_atual + 1).to_s)
   
   total_data = []
+  # total_data << collect_data
   loop do
     total_data << collect_data
-    pagina_atual = @browser.link(class: 'js-change-page', data_active: true).data_page.to_i
     if pagina_atual == total_pages
       break
     end
@@ -71,31 +75,43 @@ def viva_real
     sleep(2)
     pagina_atual = @browser.link(class: 'js-change-page', data_active: true).data_page.to_i
     proxima_pagina = @browser.link(class: 'js-change-page', data_page: (pagina_atual + 1).to_s)
+    total_pages = @browser.lis(class: 'pagination__item', data_type: 'number').last.link.data_page.to_i
   end
+  total_data = total_data.flatten
   
-  # binding.pry
+  p = Axlsx::Package.new
+  wb = p.workbook
+  wb.add_worksheet(:name => 'Planilha 1') do |sheet|
+    sheet.add_row ['Área', 'Quartos', 'Suítes', 'Banheiros', 'Vagas', 'Preço', 'Condominio', 'Link']
+    total_data.each do |item|
+      sheet.add_row [item[:area], item[:rooms], item[:suites], item[:bathrooms], item[:garage_spot], item[:price], item[:condominio], item[:link]]
+    end
+  end
+  p.serialize('result.xlsx')
 end
 
 def collect_data
   page_data = []
+  @browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
   for i in 0..(@browser.divs(data_type: 'property').length-1)
     properties = @browser.divs(data_type: 'property')[i]
+    link = properties.link.href.split('?__vt')[0]
     
     info = properties.div(class: 'property-card__main-content').ul(class: 'property-card__details')
-    area = info.li(class: 'property-card__detail-area').text
-    rooms = info.li(class: 'js-property-detail-rooms').text
-    suites = info.li(class: 'js-property-detail-suites').text
-    bathrooms = info.li(class: 'js-property-detail-bathroom').text
-    garage_spot = info.li(class: 'js-property-detail-garages').text
+    area = info.li(class: 'property-card__detail-area').text.to_i
+    rooms = info.li(class: 'js-property-detail-rooms').text.to_i
     
+    suites = info.li(class: 'js-property-detail-suites').text.to_i
+    bathrooms = info.li(class: 'js-property-detail-bathroom').text.to_i
+    garage_spot = info.li(class: 'js-property-detail-garages').text.to_i
     values = properties.div(class: 'property-card__main-content').section(class: 'property-card__values')
-    price = values.div(class: 'js-property-card-prices').text
-    if values.div(class: 'property-card__price-details--condo').exist?
-      condominio = values.div(class: 'property-card__price-details--condo').text.split('R$ ')[1]
-    else
-      condominio = ''
-    end
+    price = values.div(class: 'js-property-card-prices').text.gsub(/\D/, '').to_i
     
+    if values.div(class: 'property-card__price-details--condo').exist?
+      condominio = values.div(class: 'property-card__price-details--condo').text.gsub(/\D/, '').to_i
+    else
+      condominio = 0
+    end
     page_data[i] = {
       area: area,
       rooms: rooms,
@@ -103,10 +119,11 @@ def collect_data
       bathrooms: bathrooms,
       garage_spot: garage_spot,
       price: price,
-      condominio: condominio
+      condominio: condominio,
+      link: link
     }
   end
-  return page_data
+  page_data
 end
 
 viva_real
